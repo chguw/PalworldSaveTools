7b98d80867f1cdb0fee7f3f90b232fbbac55a78a
CyrixJD115 <129636984+CyrixJD115@users.noreply.github.com>
2026-06-06 12:31:37 -0500

    Remove old pyooz directory (replaced by palooz)

--- src/palsav/pyooz/ooz/dep/ooz/murmur.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-uint64_t murmur_hash_64a(const void *key, int len, uint64_t seed);
--- src/palsav/pyooz/ooz/dep/ooz/path_rep.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> generate_paths(void const* spec_data, size_t spec_size);
-void explain_paths(void const* spec_data, size_t spec_size);
